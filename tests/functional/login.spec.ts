a357501e4eb85ea315bf2c41805cf76e2cc953ac
sureshkct <sureshkct@gmail.com>
2026-05-31 15:21:56 -0400

    Refactor the Code

--- tests/functional/login.spec.ts
+++ tests/functional/login.spec.ts
@@ -1,40 +1,39 @@
 import { test, expect } from '@playwright/test';
 
-test('should allow user to log in with valid credentials', async ({ page }) => {
-    await page.goto('https://katalon-demo-cura.herokuapp.com/');
-    await expect(page).toHaveTitle('CURA Healthcare Service');
-    await expect(page.locator('h1')).toHaveText('CURA Healthcare Service');
-    //Click to Make a Appointment
 
-    await page.getByRole('link', { name: 'Make Appointment' }).click();
-    await expect(page.getByText("Please login to make appointment.")).toBeVisible();
+test.describe('Login Functionality', () => {
 
-    // Login
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('John Doe');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('ThisIsNotAPassword');
-    await page.getByRole('button', { name: 'Login' }).click();
+    test.beforeEach("Go to Login Page", async ({ page }) => {
+        await page.goto('https://katalon-demo-cura.herokuapp.com/');
+        await expect(page).toHaveTitle('CURA Healthcare Service');
+        await expect(page.locator('h1')).toHaveText('CURA Healthcare Service');
+        //Click to Make a Appointment
+        await page.getByRole('link', { name: 'Make Appointment' }).click();
+        await expect(page.getByText("Please login to make appointment.")).toBeVisible();
+    });
 
-    // Verify successful login
-    await expect(page.locator('#appointment')).toBeVisible();
-});
+    test('should allow user to log in with valid credentials', async ({ page }) => {
 
-test('Negative: should display error message with invalid credentials', async ({ page }) => {
-    await page.goto('https://katalon-demo-cura.herokuapp.com/');
-    await expect(page).toHaveTitle('CURA Healthcare Service');
-    await expect(page.locator('h1')).toHaveText('CURA Healthcare Service');
-    //Click to Make a Appointment
-    await page.getByRole('link', { name: 'Make Appointment' }).click();
-    await expect(page.getByText("Please login to make appointment.")).toBeVisible();
+        // Login
+        await page.getByLabel('Username').click();
+        await page.getByLabel('Username').fill('John Doe');
+        await page.getByLabel('Password').click();
+        await page.getByLabel('Password').fill('ThisIsNotAPassword');
+        await page.getByRole('button', { name: 'Login' }).click();
+        // Verify successful login
+        await expect(page.locator('#appointment')).toBeVisible();
+    });
 
-    // Login
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('John Smith');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('ThisIsNotAPassword');
-    await page.getByRole('button', { name: 'Login' }).click();
-    //Assert error message is displayed
-    await expect(page.getByText('Login failed! Please ensure the username and password are valid.')).toBeVisible();
-    // Verify successful login
-});
+    test('Negative: should display error message with invalid credentials', async ({ page }) => {
+
+        await page.getByLabel('Username').click();
+        await page.getByLabel('Username').fill('John Smith');
+        await page.getByLabel('Password').click();
+        await page.getByLabel('Password').fill('ThisIsNotAPassword');
+        await page.getByRole('button', { name: 'Login' }).click();
+        //Assert error message is displayed
+        await expect(page.getByText('Login failed! Please ensure the username and password are valid.')).toBeVisible();
+        // Verify successful login
+    });
+}
+);
